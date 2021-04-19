bebc7c1fe84016a688bfa96b0189ef5970398637
spyrosbazios <p3170113@aueb.gr>
2021-04-20 01:48:31 +0300

    game selection screen started

---
+++ front-end/tyche.js
@@ -0,0 +1,7 @@
+function hoverBoxInfo(element) {
+    element.setAttribute('src', "assets/box-info.png");
+  }
+
+  function unhoverBoxInfo(element) {
+    element.setAttribute('src', "assets/box.png");
+  }
