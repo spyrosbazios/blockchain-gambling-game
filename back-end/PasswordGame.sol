ea6a9038322b12427a878d406d4346ff6e2444ed
chrisargy <chrisargy@hotmail.gr>
2021-04-29 15:07:27 +0300

    added comments, changed a few method names

--- back-end/PasswordGame.sol
+++ back-end/PasswordGame.sol
@@ -16,50 +16,60 @@ contract PasswordGame {
     mapping (address => Bet) bets;
     mapping (address => bool) hasBet;
     
-    function bet(uint amount, 
-                 uint8 c11, uint8 c12, uint8 c13,
-                 uint8 c21, uint8 c22, uint8 c23,
-                 uint8 c31, uint8 c32, uint8 c33)
+    /* create a bet for a player */
+    function createBet(uint amount, 
+                       uint8 c11, uint8 c12, uint8 c13,
+                       uint8 c21, uint8 c22, uint8 c23,
+                       uint8 c31, uint8 c32, uint8 c33)
     public {
-       
+        
         require(msg.sender.balance >= amount && !hasBet[msg.sender]);
         require (uintInRange(c11) && uintInRange(c12) && uintInRange(c13), "first code not in range!");
         require (uintInRange(c21) && uintInRange(c22) && uintInRange(c23), "second code not in range!");
         require (uintInRange(c31) && uintInRange(c32) && uintInRange(c33), "third code not in range!");
         
-        uint code1 = uintToDigit3(c11, c12, c13);
-        uint code2 = uintToDigit3(c21, c22, c23);
-        uint code3 = uintToDigit3(c31, c32, c33);
+        uint code1 = uintTo3digit(c11, c12, c13);
+        uint code2 = uintTo3digit(c21, c22, c23);
+        uint code3 = uintTo3digit(c31, c32, c33);
         bets[msg.sender] = Bet(amount, block.number, code1, code2, code3);
         hasBet[msg.sender] = true;
         
         /* ... transfer money ... */
     }
     
-    function verify() public {
+    /* check if a bet has won */
+    function verifyBet() public returns (bool) {
         require (hasBet[msg.sender]);
         require (bets[msg.sender].blockNumber == block.number - 1);
         
+        bool won;
         Bet memory b = bets[msg.sender];
-        if (verify(b.code1) || verify(b.code2) || verify(b.code3)) {
+        if (verifyCode(b.code1) || verifyCode(b.code2) || verifyCode(b.code3)) {
+            won = true;
             /* ... you have won ... */
         } else {
+            won = false;
             /* ... you have lost ... */
-        }
-        
+        }   
         hasBet[msg.sender] = false;
+        
+        return won;
     }
     
-    function verify(uint i) private view returns (bool) {
-        uint x = uint(keccak256(abi.encodePacked(i, blockhash(block.number - 1))));
+    /* check if a code has won */
+    function verifyCode(uint code) private view returns (bool) {
+        //uint x = uint(keccak256(abi.encodePacked(i, blockhash(block.number - 1))));
+        uint x = uint(keccak256(bytes.concat(bytes32(code), blockhash(block.number - 1))));
         return x % chance == 0;
     }
     
+    /* every digit must be between 1 and 9 */
     function uintInRange(uint8 i) private pure returns (bool) {
         return i >= 1 && i <= 9;
     }
     
-    function uintToDigit3 (uint8 e, uint8 d, uint8 m) private pure returns (uint) {
+    /* converts 3 different digits to a single 3-digit number */
+    function uintTo3digit (uint8 e, uint8 d, uint8 m) private pure returns (uint) {
         return uint(m + d * 10 + e * 100);
     }
 
@@ -67,7 +77,7 @@ contract PasswordGame {
 
 /* 
 to-do:
-add winning amount per bet 
+add winning amount per bet
 add mapping index of every bet so I can remove the bet from the mapping after its verification
 */
 
