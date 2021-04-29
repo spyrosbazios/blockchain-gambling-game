ba38aad922324ba8dd85e12e52e744afe8cce37b
spyrosbazios <p3170113@aueb.gr>
2021-04-29 12:02:26 +0300

    front end v1 deleted

--- front-end/tyche.js
+++
@@ -1,8 +0,0 @@
-function hoverBoxInfo(element) {
-    element.setAttribute('src', "assets/box-info.png");
-  }
-
-  function unhoverBoxInfo(element) {
-    element.setAttribute('src', "assets/box.png");
-  }
-  
