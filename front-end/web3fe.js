dd59f88b363e16386af6b2b9a3b75e997bb97916
chrisargy <chrisargy@hotmail.gr>
2021-05-21 22:38:00 +0300

    minor changes for better readability

--- front-end/web3fe.js
+++ front-end/web3fe.js
@@ -2,32 +2,25 @@
 	if (web3) {
 		try {
 			await window.ethereum.send('eth_requestAccounts');
-			//web3.eth.getAccounts().then(e => let firstAcc=e[0]; console.log(firstAcc););
 			return true;
 		}
 		catch(e) {return false;}
 	}
 }
 
-async function test() {
+document.getElementById('connect_wallet').onclick = async function connectWallet() {
+	console.log('Connecting wallet ...');
 	const result = await ethEnabled();
-	console.log('Wallet connection: ' + result);
-}
+	console.log('Wallet connected: ' + result);
+};
 
-window.onload = function() {
-	console.log('Connection wallet ...');
-	test();		
-}
-
-
-
-/*var contract_abi = require('/Users/macbook/Sync/Information Systems Dev/blockchain-gambling-game/front-end/abi.json');
+/*
+var contract_abi = require('/Users/macbook/Sync/Information Systems Dev/blockchain-gambling-game/front-end/abi.json');
 var contract_address = '0xF28Dfa8306AF0804E20355ED262B42a6aB28aDCB';
 const passwordGameContract = web3.eth.Contract(contract_abi, contract_address);
-
 */
 
-function bet(){
+function bet() {
 	passwordGameContract.methods.createBet(1,[1,2,3,4,5,6,7,8,9]);
 	console.log('contract method called')
 }
