072b9f2cb8387cf4088c39582ba645f466a2bc9e
Chara <chara.gergi@gmail.com>
2021-06-03 16:05:07 +0300

    double code was deleted

--- front-end/game-play/box-game.js
+++ front-end/game-play/box-game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* 
  * 
  * SLIDE TO UNCLOCK FUNCTIONALITY 
@@ -160,60 +159,3 @@ function getBtnId(btnId){
 
   }
 }
-=======
-/* 
- * 
- * SLIDE TO UNCLOCK FUNCTIONALITY 
- * 
- */
-
-var swiperDragged = false,
-startX,
-endX = 0;
-
-function swipe(){
-var $swipe = $('.swiper'),
-  $btn = $('.swipe-btn', $swipe);
-
-TweenLite.to('#swipe-arrow', 0, { x: 16, y: 11});
-TweenLite.to('#swipe-end', 0, { x: 235, y: 12});
-
-var tl = new TimelineMax({ repeat: -1 });
-tl.staggerFrom("#dotted-line circle", 0.7, { scale: 0.7, x: -2, y: .5, opacity: 0.7, delay:0.1, ease: Power2.easeInOut, repeat: 1, yoyo: true}, 0.15);
-
-$btn.on('click touchend', function(e){
-e.preventDefault();
-}).on('touchstart mousedown', function(e) {
-e.preventDefault();
-swiperDragged = true;
-startX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
-endX = 0;
-})
-
-$(document).on('touchmove mousemove', function(e){
-if (swiperDragged) {
-  actualX = typeof e.pageX != 'undefined' ? e.pageX : e.originalEvent.touches[0].pageX;
-  endX = Math.max(0, Math.min(215, actualX - startX));
-  TweenLite.to('#swipe-btn', 0, { x: endX});
-}
-}).on('touchend mouseup', function(e) {
-if (swiperDragged) {
-  swiperDragged = false;
-  if (endX < 200) {
-    TweenLite.to('#swipe-btn', .5, { x: 0 });
-  } else {
-    console.log($btn.attr('xlink:href'));
-    TweenLite.to('#swipe-btn', .1, { x: 215});
-    $('.unlock').addClass('unlocked');
-    setTimeout(function(){
-      TweenLite.to('#swipe-btn', .5, { x: 0 });
-      $('.unlock').removeClass('unlocked');
-    }, 1400);
-  }
-  endX = 0;
-}
-});
-}
-
-swipe();
->>>>>>> 4b61759179d066b43460b3a1302183a158e9caed
